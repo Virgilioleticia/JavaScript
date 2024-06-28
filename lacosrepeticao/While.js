5847494b58f850680d8013a8fdb50edc1c795d7b
Letícia <virgilioleticia240902@gmail.com>
2024-06-28 13:54:02 -0300

    Lista_03

--- lacosrepeticao/While.js
+++ lacosrepeticao/While.js
@@ -1,21 +1,20 @@
-const leia = require('readline-sync');
+const leia = require("readline-sync")
 
-let continua; true;
+let continua;
 let numero1, numero2, resultado;
 
-console.log("\n++++++++++++++++MENU++++++++++++++++++");
-continua = leia.keyInYNStrict("\nDeseja continuar? ");
-console.log("++++++++++++++++++++++++++++++++++");
+console.log("\n++++++++++++++++MENU++++++++++++++++");
+continua = leia.keyInYNStrict("\nDeseja usar nossa calculadora de soma? ")
+console.log("++++++++++++++++++++++++++++++++")
 
 while(continua){
-    numero1 = leia.questionInt("Digito o primeiro valor ");
-    numero2 = leia.questionInt("digite o segundo valor:");
+    numero1 = leia.questionInt("Digite o primeiro valor: ")
+    numero2 = leia.questionInt("Digite o segundo valor: ")
 
-    resultado = numero1 + numero2
+    resultado = numero1 + numero2;
     console.log("O resultado da soma é: " + resultado);
 
-    console.log("\n++++++++++++++++MENU++++++++++++++++++");
-    continua = leia.keyInYNStrict("\nDeseja continuar? ");
-    console.log("++++++++++++++++++++++++++++++++++");
-
+    console.log("\n++++++++++++++++MENU++++++++++++++++");
+    continua = leia.keyInYNStrict("\nDeseja continuar? ")
+    console.log("++++++++++++++++++++++++++++++++")
 }
